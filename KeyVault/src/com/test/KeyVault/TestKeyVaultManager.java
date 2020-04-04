95ec0298c62a27f1db2b843855d8815c79683a7d
Rupesh Jha <58739118+itsrupesh-tech@users.noreply.github.com>
2020-04-04 20:10:08 +0530

    updated keyVault

--- KeyVault/src/com/test/KeyVault/TestKeyVaultManager.java
+++ KeyVault/src/com/test/KeyVault/TestKeyVaultManager.java
@@ -10,13 +10,7 @@ public static void main(String[] args) {
 
 
     ManageKeyVault manageKeyVault = new ManageKeyVault();
-   /*String clientId = "1e47a2ca-9475-43f6-99c9-6d3762658cf0";
-    String clientSecret = "ed5cc292-1c22-4e85-ae03-19bef906b465";
-    String tenantId = "7f2c1900-9fd4-4b89-91d3-79a649996f0a";
-
-     String vaultURL = "https://kv-sg01-sea-u-jfw-vlt01.vault.azure.net/";
-     String secretName = "ipos-db";*/
-
+   
 
 		
 		  String clientId = "09e72b93-ac71-44fb-896a-26edb211e265"; 
